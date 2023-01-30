8e05ac8c83e24375281f1ee1e00aca3ce23e4c3e
“markantonio99” <markomakkia@gmail.com>
2023-01-30 16:57:04 +0100

    Sal 3

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let titleEelement= document.getElementById("main-title")
+
+titleEelement.innerHTML= "benvenuti"
+
+
+
+
+
+nome= prompt('Come ti chiami?')
+
+cognome= prompt('E il tuo cognome?')
+
+colore= prompt("Il tuo colore preferito?")
+
+ 
+
+titleEelement.innerHTML=  'la tua password è:' + ' ' + nome + cognome + colore
+
+
+ 
